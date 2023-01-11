e8aaebaa3406750a27be69a73213bc7674829941
nagendra212001 <nagendra212001@gmail.com>
2023-01-11 21:04:10 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class School(models.Model):
+    school=models.CharField(max_length=100)
+    principal=models.CharField(max_length=100)
+
+class Student(models.Model):
+    school=models.ForeignKey(School,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    
